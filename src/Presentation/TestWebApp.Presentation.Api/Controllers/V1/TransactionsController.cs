85ece43e1fca3f4ea8578f1dd4e545e925d3ba89
Marin Jansky <marin.jansky@rokolabs.com>
2023-08-29 16:56:43 +0200

    Transactions controller

---
+++ src/Presentation/TestWebApp.Presentation.Api/Controllers/V1/TransactionsController.cs
@@ -0,0 +1,46 @@
+﻿namespace TestWebApp.Presentation.Api.Controllers.V1
+{
+    using System;
+
+    using MediatR;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using TestWebApp.Application.Transactions.Commands;
+    using TestWebApp.Application.Transactions.Common;
+    using TestWebApp.Application.Transactions.Queries;
+    using TestWebApp.Presentation.Api.Internal.Constants;
+
+    [ApiVersion(ApiVersions.V1)]
+    public class TransactionsController : ApiControllerBase
+    {
+        public TransactionsController(IMediator mediator) : base(mediator) { }
+
+        [AllowAnonymous]
+        [HttpPost("create")]
+        public async Task<IActionResult> Create([FromBody] CreateTransactionCommand command)
+        {
+            return await this.ProcessAsync(command);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("with-account")]
+        public async Task<IActionResult> WithAccount([FromQuery] GetTransactionsWithAccountQuery query)
+        {
+            return await this.ProcessAsync<GetTransactionsWithAccountQuery, List<TransactionResponse>>(query);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("with-user")]
+        public async Task<IActionResult> WithUser([FromQuery] GetTransactionsWithUserQuery query)
+        {
+            return await this.ProcessAsync<GetTransactionsWithUserQuery, List<TransactionResponse>>(query);
+        }
+
+        [AllowAnonymous]
+        [HttpGet("transaction")]
+        public async Task<IActionResult> Get([FromQuery] GetTransactionByIdQuery query)
+        {
+            return await this.ProcessAsync<GetTransactionByIdQuery, TransactionResponse>(query);
+        }
+    }
+}
