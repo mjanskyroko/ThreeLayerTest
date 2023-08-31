06194b63b3d02a6ac9a853095e64549bebc3779c
Marin Jansky <marin.jansky@rokolabs.com>
2023-08-31 10:08:14 +0200

    Remaining ED configurations

---
+++ src/Infrastructure/TestWebApp.Infrastructure.Database.Mssql/Internal/Configurations/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace TestWebApp.Infrastructure.Database.Mssql.Internal.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using System;
+    using TestWebApp.Domain;
+
+    internal sealed class AccountEntityTypeConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("Accounts");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Name).HasMaxLength(32).IsRequired();
+            builder.Property(a => a.Balance).IsRequired();
+            builder.HasOne(a => a.Owner).WithMany().HasForeignKey(a => a.Id);
+        }
+    }
+}
---
+++ src/Infrastructure/TestWebApp.Infrastructure.Database.Mssql/Internal/Configurations/TransactionEntityTypeConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace TestWebApp.Infrastructure.Database.Mssql.Internal.Configurations
+{
+    using System;
+
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using TestWebApp.Domain;
+
+    internal class TransactionEntityTypeConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Amount).IsRequired();
+            builder.HasOne(t => t.From).WithMany().HasForeignKey(t => t.Id);
+            builder.HasOne(t => t.To).WithMany().HasForeignKey(t => t.Id);
+        }
+    }
+}
