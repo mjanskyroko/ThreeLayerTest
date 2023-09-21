10b8bfbb90f0a09f13fbf1c79d23483f8b7b66c1
Marin Jansky <marin.jansky@rokolabs.com>
2023-09-21 13:50:58 +0200

    Fixed tests not passing because of parallel execution

---
+++ tests/TestWebApp.Infrastructure.Database.Mssql.Tests/Main.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass, DisableTestParallelization = true)]
+
+namespace TestWebApp.Infrastructure.Database.Mssql.Tests
+{
+    
+}
